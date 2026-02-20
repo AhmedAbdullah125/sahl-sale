a38a41478227ef239c03d826fd8643d35e9f6945
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2026-02-20 10:53:29 +0200

    update

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,16 @@
-import Image from 'next/image'
-import logo from '../../images/freeLogo.png'
+import Image from "next/image";
+
 export default function Profile() {
-
     return (
-
         <div className="img-empty">
-            <Image src={logo} alt='B3' className='logo-in-profile'></Image>
+            <Image
+                src="/images/freeLogo.png"
+                alt="B3"
+                className="logo-in-profile"
+                width={200}
+                height={200}
+                priority
+            />
         </div>
-    )
+    );
 }
