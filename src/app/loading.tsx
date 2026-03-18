7ea906fed4491f9322e0e89e5b741774327923d9
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2026-03-18 22:29:12 +0200

    update

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,8 +3,29 @@ import Image from "next/image";
 
 const Loader = () => {
   return (
-    <div className="loading-container">
-      <Image src={logo} alt="Logo" width={100} height={100} className="object-contain" priority />
+    <div className="loading-contt">
+
+      <svg className="loader" viewBox="0 0 384 384" xmlns="http://www.w3.org/2000/svg">
+        <circle
+          className="active"
+          pathLength="360"
+          fill="transparent"
+          stroke-width="32"
+          cx="192"
+          cy="192"
+          r="176"
+        ></circle>
+        <circle
+          className="track"
+          pathLength="360"
+          fill="transparent"
+          stroke-width="32"
+          cx="192"
+          cy="192"
+          r="176"
+        ></circle>
+      </svg>
+
     </div>
   );
 }
